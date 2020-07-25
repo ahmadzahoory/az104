b653cc296272f6866a356c803ef482cd1ef8ea84
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-07-25 21:57:09 +0530

    Add files via upload

---
+++ lab-104-06-03-a.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt-get -y update
+apt-get install -y apache2
+apt-get install -y unzip
+systemctl start apache2.service
+cd /var/www/html/
+rm index.html
+wget https://raw.githubusercontent.com/ahmadzahoory/az103template/master/az-104-06-03-code-a.zip
+unzip az-104-06-03-code-a.zip
